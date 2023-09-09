c6360327b41984c263c9efa665bf53fcad1e9b0f
zzy <528593591@qq.com>
2023-09-09 18:33:39 +0800

    thi commit

--- src/main/java/Gittes.java
+++ src/main/java/Gittes.java
@@ -2,6 +2,7 @@ public class Gittes {
     public static void main(String[] args) {
         System.out.println("bbb");
         System.out.println("cc");
+        System.out.println("dd");
 
     }
 }
